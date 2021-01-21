50750e66559ad051524978c0fe781b04b36c4ecb
Casper <csl@netcompany.com>
2021-01-21 13:53:55 +0100

    Simple app

---
+++ ApplikationSimple.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="applikation-container">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Applikation'
+})
+export default class Applikation extends Vue {
+
+
+  mounted() {
+    alert('hej');
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
