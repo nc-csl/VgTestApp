45b309efa5ea04171c91cbe5bc955797adbc34f1
Casper <csl@netcompany.com>
2021-01-22 10:13:47 +0100

    Add options api

---
+++ ApplikationOptionsApi.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="applikation-container">
+
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+
+    }
+  },
+  mounted(){
+    alert('hej');
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
